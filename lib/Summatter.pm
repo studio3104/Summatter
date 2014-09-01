b19fe5f4864f87510749200875d1c0e1a7f4f39a
studio3104 <studio3104.com@gmail.com>
2014-09-01 14:10:50 +0900

    initial import

---
+++ lib/Summatter.pm
@@ -0,0 +1,47 @@
+package Summatter;
+use strict;
+use warnings;
+use utf8;
+our $VERSION='0.01';
+use 5.008001;
+use Summatter::DB::Schema;
+use Summatter::DB;
+
+use parent qw/Amon2/;
+# Enable project local mode.
+__PACKAGE__->make_local_context();
+
+my $schema = Summatter::DB::Schema->instance;
+
+sub db {
+    my $c = shift;
+    if (!exists $c->{db}) {
+        my $conf = $c->config->{DBI}
+            or die "Missing configuration about DBI";
+        $c->{db} = Summatter::DB->new(
+            schema       => $schema,
+            connect_info => [@$conf],
+            # I suggest to enable following lines if you are using mysql.
+            # on_connect_do => [
+            #     'SET SESSION sql_mode=STRICT_TRANS_TABLES;',
+            # ],
+        );
+    }
+    $c->{db};
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Summatter - Summatter
+
+=head1 DESCRIPTION
+
+This is a main context class for Summatter
+
+=head1 AUTHOR
+
+Summatter authors.
+
---
+++ t/00_compile.t
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+use Summatter;
+use Summatter::Web;
+use Summatter::Web::View;
+use Summatter::Web::ViewFunctions;
+
+use Summatter::DB::Schema;
+use Summatter::Web::Dispatcher;
+
+
+pass "All modules can load.";
+
+done_testing;
